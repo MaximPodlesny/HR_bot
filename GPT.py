c733d59de8bcb9f9e53195b545124866657f5f92
MaximPodlesny <max070730@gmail.com>
2024-10-18 21:27:35 +0300

    with poll

--- GPT.py
+++ GPT.py
@@ -1,5 +1,6 @@
 import asyncio
 from handlers.search_candidate import search_c
+from handlers.utils.candidate import collect_candidate_info
 # from openai import OpenAI, AsyncOpenAI
 # from config import GPT_KEY
 
@@ -45,6 +46,7 @@ async def process_commitment(message):
   prompt = "Ты  -  умный  и  дружелюбный  HR-бот,  который  помогает  пользователям  найти  вакансии,  отправить  резюме,  пройти  собеседование  и  получить  тестовое  задание.  Ты  интегрирован  с  API  HeadHunter,  Bitrix24  и  OpenAI.\
               **Твои  основные  задачи:**\
               *   **Поиск  кандидатов на вакансию:**  Помоги  найти кандидаов на вакансии  через  HeadHunter  или собственную базу резюме.\
+              *   **Если ты получаешь недостаточно информации о кандидате(Каким вы видите идеального кандидата?, Пол / возраст / минимальный опыт?, Какие у него должны быть качества?, Какими навыками должен обладать?), необходимо вызвать функцию 'collect_candidate_info()'\
               *   **Применение функций:**  Для выполнения поставленных задачь обязательно применяй следующие функции: 'search_c()' - для составления портрета кандидата, 'create_vacancy()' - для создания вакансии на HeadHunter.\
               *   **Отправка  вакансии:**  Помоги  отправить вакансию  на  HeadHunter  или  Bitrix24.\
               *   **Создание  лидов  в  Bitrix24:**  Создавай  новые  лиды  в  Bitrix24  для  кандидатов,  которые  связались  с  ботом.\
@@ -63,8 +65,37 @@ async def process_commitment(message):
          {"role": "system", "content": prompt},
          {"role": "user", "content": message}
          ],
-      # Add any other necessary parameters
+        #  functions=[
+        #     {
+        #         "name": "collect_candidate_info",
+        #         "description": "Собирает информацию о кандидате на вакансию.",
+        #         "parameters": {
+        #             "type": "object",
+        #             "properties": {
+        #                 "a": {"type": "integer"},
+        #                 "b": {"type": "integer"},
+        #             },
+        #             "required": ["a", "b"],
+        #         },
+        #     }
+          # ]      # Add any other necessary parameters
   )
+  # if response.choices[0].message.function_call:
+  #      # Получение аргументов функции
+  #     #  function_args = response.choices[0].message.function_call.arguments
+
+  #      # Вызов функции
+  #      result = await collect_candidate_info()
+
+  #      # Отправка ответа ChatGPT
+  #      response = await client.chat.completions.async_create(
+  #         model= g4f.models.gpt_4o_mini,
+  #         messages=[
+  #             {"role": "system", "content": prompt},
+  #             {"role": "user", "content": message},
+  #             {"role": "function", "name": "calculate_sum", "content": result},
+  #         ],
+  #      )
 
   # await message.answer(response.choices[0].message.content)
   return response.choices[0].message.content
