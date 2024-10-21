acfdb35033c58606ecab7f481faae1e4236b1e99
MaximPodlesny <max070730@gmail.com>
2024-10-21 21:41:32 +0300

    with db all 21.10

---
+++ handlers/create_vacancy.py
@@ -0,0 +1,155 @@
+import logging
+from typing import Any, Dict
+from aiogram import Bot, Dispatcher, types
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+from db.create_table import Vacancies
+import psycopg2
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+# from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
+from .utils.candidate import CandidateInfoStates
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+logging.basicConfig(level=logging.INFO)
+keyboard = ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    types.KeyboardButton(text="Отмена"),
+                ],
+            ],
+            resize_keyboard=True
+        )
+
+class VacancyInfoStates(StatesGroup):
+    waiting_for_create_portrait = State()
+    waiting_for_title = State()
+    waiting_for_conditions = State()
+    waiting_for_requirements = State()
+    waiting_for_responsibilities = State()
+    waiting_for_interview_questions = State()
+    waiting_for_priority = State()
+
+# 3.  Функция  для  создания  соединения  с  PostgreSQL:**
+async def get_db():
+    async_engine = create_async_engine(DATABASE_URL, echo=True)  # Echo=True для вывода SQL-запросов
+    async_session = async_sessionmaker(
+        async_engine, expire_on_commit=False, class_=AsyncSession
+    )
+    async with async_session() as session:
+        yield session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# 4.  Функция  для  сохранения  информации  о  кандидате  в  базу:**
+# async def save_vacancy_info(data: Dict[str, Any]):
+#     """Сохраняет информацию о кандидате в базу данных."""
+#     async with get_db() as session:
+#         # Работа с базой данных внутри контекста сессии
+#         new_vacancy = Vacancies(title=data["waiting_for_title"], conditions=data["waiting_for_conditions"], requirements=data["waiting_for_requirements"], responsibilities=data["waiting_for_responsibilities"], interview_questions=data["waiting_for_interview_questions"], priority=data["waiting_for_priority"])
+#         session.add(new_vacancy)
+#         await session.commit()
+def save_vacancy_info(data: dict):
+    """Сохраняет информацию о вакансии в базу данных."""
+    title = data["waiting_for_title"]
+    existing_vacancy = session.query(Vacancies).filter_by(title=title).first()
+    if existing_vacancy:
+        print(f"Вакансия с названием '{title}' уже существует.")
+        return
+    else:
+        new_vacancy = Vacancies(
+            title=data["waiting_for_title"],
+            conditions=data["waiting_for_conditions"],
+            requirements=data["waiting_for_requirements"],
+            responsibilities=data["waiting_for_responsibilities"],
+            interview_questions=data["waiting_for_interview_questions"],
+            priority=data["waiting_for_priority"],
+        )
+        session.add(new_vacancy)
+        session.commit()
+
+# 5.  Функция  для  сбора  информации  о  кандидате:
+async def collect_vacancy_info(message: types.Message, state: FSMContext):
+    print(
+        "in collect_vacancy_info"
+    )
+    global keyboard
+    if await state.get_state() == VacancyInfoStates.waiting_for_title:
+        await state.update_data(waiting_for_title=message.text) # (waiting_for_conditions=message.text)
+        await state.set_state(VacancyInfoStates.waiting_for_conditions)
+        await message.reply("Опишите условия работы: график, зп, удаленно/офлайн, бонусы, KPI?", reply_markup=keyboard)
+    elif await state.get_state() == VacancyInfoStates.waiting_for_conditions:
+        await state.update_data(waiting_for_conditions=message.text)
+        await state.set_state(VacancyInfoStates.waiting_for_requirements)
+        await message.reply("Какие требования к кандидату?", reply_markup=keyboard)
+    elif await state.get_state() == VacancyInfoStates.waiting_for_requirements:
+        await state.update_data(waiting_for_requirements=message.text)
+        await state.set_state(VacancyInfoStates.waiting_for_responsibilities)
+        await message.reply("Какие обязанности будут у кандидата?", reply_markup=keyboard)
+    elif await state.get_state() == VacancyInfoStates.waiting_for_responsibilities:
+        await state.update_data(waiting_for_responsibilities=message.text)
+        await message.reply("Какие вопросы зададите на собеседовании?")
+        await state.set_state(VacancyInfoStates.waiting_for_interview_questions)
+        # await message.reply("Какие обязанности будут у кандидата?", reply_markup=keyboard)
+    elif await state.get_state() == VacancyInfoStates.waiting_for_interview_questions:
+        await state.update_data(waiting_for_interview_questions=message.text)
+        await state.set_state(VacancyInfoStates.waiting_for_priority)
+        await message.reply("На что вы будете обращать внимание при финальном выборе?", reply_markup=keyboard)
+    elif (await state.get_state() == VacancyInfoStates.waiting_for_priority) and ((await state.get_data())["waiting_for_create_portrait"] != 'создать'):
+        await state.update_data(waiting_for_priority=message.text)
+        data = await state.get_data()
+        await message.reply("Информация о вакансии собрана!")
+        save_vacancy_info(data=data)
+        # await save_candidate_info(message.chat.id, data)
+        await show_summary(message=message, data=data)
+        await state.clear()
+    elif (await state.get_state() == VacancyInfoStates.waiting_for_priority) and ((await state.get_data())["waiting_for_create_portrait"] == 'создать'):
+        await state.update_data(waiting_for_priority=message.text)
+        data = await state.get_data()
+        save_vacancy_info(data=data)
+        await message.reply("Информация о вакансии собрана!")
+        await show_summary(message=message, data=data)
+        await message.answer('Хорошо! Давайте соберем информацию о кандидате.')
+        await state.set_state(CandidateInfoStates.waiting_for_ideal_candidate)
+        await state.update_data(waiting_for_title_vacancy=data["waiting_for_title"])
+        await state.set_state(CandidateInfoStates.waiting_for_ideal_candidate)
+        keyboard = ReplyKeyboardMarkup(
+                keyboard=[
+                    [
+                        types.KeyboardButton(text="Отмена"),
+                    ],
+                ], 
+                resize_keyboard=True
+        )
+        await message.reply('Каким вы видите идеального кандидата?', reply_markup=keyboard)
+        
+        # await save_candidate_info(message.chat.id, data)
+        
+        
+
+async def show_summary(message: Message, data: Dict[str, Any]) -> None:
+    waiting_for_title = data["waiting_for_title"]
+    waiting_for_conditions = data["waiting_for_conditions"]
+    waiting_for_requirements = data["waiting_for_requirements"]
+    waiting_for_responsibilities = data["waiting_for_responsibilities"]
+    waiting_for_interview_questions = data["waiting_for_interview_questions"]
+    waiting_for_priority = data["waiting_for_priority"]
+    await message.answer(
+        f"Условия работы: {waiting_for_title}\n\n"
+        f"Условия работы: {waiting_for_conditions}\n\n"
+        f"Требования: {waiting_for_requirements}\n\n"
+        f"Обязанности: {waiting_for_responsibilities}\n\n"
+        f"Вопросы на собеседовании: {waiting_for_interview_questions}\n\n"
+        f"Приоритет: {waiting_for_priority}",
+        reply_markup=ReplyKeyboardRemove(),
+    )
